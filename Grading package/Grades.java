ef6965d4154b11b8803b8ccf110d521300670737
JakeSTW <91596606+JakeSTW@users.noreply.github.com>
2021-11-02 16:54:31 +0000

    Changes
    I had to change the whole Grade class since I muddled it up

--- Grading package/Grades.java
+++ Grading package/Grades.java
@@ -1,112 +1,26 @@
-
-import java.util.ArrayList;
 /**
- * Write a description of class Course here.
- *
- * @author (Jake Stewart)
- * @version (20/10/21)
- */
-public class Grades{
-public class Course
-
+*Each value is the top percentage mark for that grade
+*@author 1.0 Jake Stewart
+*version 1.0 2/11/21
+*/
+public enum Grades
 {
-    public final static int MAXN_MODULES = 4;
+    NS (0),
+    F (39),
+    D (49),
+    C (59), 
+    B (69),
+    A (100);
     
-    public ArrayList<Module> modules;
+    private final int value;
     
-    private String code;
-    private String title;
-    
-    private Grades finalGrade;
-     
-    public Course()
+    private Grades(int value)
     {
-        this("MT1CYS1", "bsc cyber-security");
+       this.value = value; 
     }
     
-    /**
-     * Constructor for objects of class Course
-     */
-    public Course(String code, String title)
+    public int getValue()
     {
-        // initialise instance variables
-        this.code = code;
-        this.title = title;
-        
-        modules  = new ArrayList<Module>();
-        
-        createModules();
+        return value;
     }
-
-    /**
-     * Create four modules and add them to the
-     * modules list for testing purposes.  These
-     * must be your four modules.
-     */
-    public void createModules()
-    {
-
-    }
-    
-    public void addModule(Module module)
-    {
-        if(modules.size() < MAXN_MODULES)
-        {
-            modules.add(module);
-        }
-    }
-    
-    /**
-     * 
-     */
-    public Grades convertToGrade(int mark)
-    {
-        if (mark>= 0 && mark <=39)
-        return Grades.F;
-        else if (mark <= 49)
-        return Grades.D;
-        else if (mark <= 59)
-        return Grades.C;
-        else if (mark <= 69)
-        return Grades.B;
-        else if (mark <= 100)
-        return Grades.A;
-        else
-        return Grades.NS;
-    }
-    
-    /**
-     * Calculate the average mark from the four module marks
-     * and convert that into a final grade.
-     */
-    public Grades calculateGrade(ArrayList<ModuleMark> marks)
-    {
-        return Grades.NS;
-    }
-    
-    /**
-     * Prints out the details of a course and the
-     * four modules
-     */
-    public void print()
-    {
-        System.out.println();
-        System.out.println(" Course " + code + ": " + title);
-        //System.out.println();
-        
-        printModules();
-    }
-    
-    /**
-     * Print the course's four modules
-     */
-    public void printModules()
-    {
-        for (Module module : modules)
-    {
-        module.print();
-        module.printCredit();
-    }
-}
-}
 }
